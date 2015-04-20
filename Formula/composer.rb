8647d3a06f4fd7290dcc009f744d247fd9c979f1
Casey Dunham <casey.dunham@gmail.com>
2015-04-20 19:33:40 -0400

    Update composer to 1.0.0-alpha10

--- Formula/composer.rb
+++ Formula/composer.rb
@@ -5,9 +5,9 @@ require File.expand_path("../../Requirements/composer-requirement", __FILE__)
 class Composer < Formula
   homepage 'http://getcomposer.org'
   head 'https://getcomposer.org/composer.phar'
-  url 'http://getcomposer.org/download/1.0.0-alpha9/composer.phar'
-  sha1 'c0917158e84c867ce69753fcbc04eb7c45bb58bb'
-  version '1.0.0-alpha9'
+  url 'http://getcomposer.org/download/1.0.0-alpha10/composer.phar'
+  sha1 '5913d2b2a0cb07e9a2d620bd1f66340ec676e28a'
+  version '1.0.0-alpha10'
 
   depends_on PhpMetaRequirement
   depends_on ComposerRequirement
